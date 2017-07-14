41f63500b5fa26f6b944a6245dd3e792d10182da
Hesham Adel <hadel@sharpmobile.ca>
2017-07-14 18:12:17 +0200

    basic app

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const {app, BrowserWindow} = require('electron'); 
 const path = require('path'); 
 const url = require('url'); 
+const Promise = require('bluebird'); 
+const fs = require('fs');
+const readFile = Promise.promisify(fs.readFile); 
+const co = require('co'); 
 
 let win
+
 
 function createWindow() {
 	win = new BrowserWindow({width: 800, height: 600}); 
 
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, '/welcome/public/index.html'), 
+		pathname: path.join(__dirname, '/index.html'), 
 		protocol: 'file', 
 		slashes: true
 	})); 
+
+	co(function* (){
+		try {
+			const indexFileData = yield readFile(path.join(__dirname, '/index.html'), 'utf8');
+			console.log(indexFileData); 
+		} catch (err) {
+			console.error(err); 
+		}
+	}); 	
 
 
 	win.webContents.openDevTools(); 
